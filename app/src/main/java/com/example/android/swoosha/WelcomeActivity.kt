85bb75f137d91edbcd0de1236b1f0552d132013f
Raju Selvaraj <rjreg7@gmail.com>
2021-05-26 16:25:14 +0530

    lession-03

--- app/src/main/java/com/example/android/swoosha/WelcomeActivity.kt
+++ app/src/main/java/com/example/android/swoosha/WelcomeActivity.kt
@@ -1,11 +1,19 @@
 package com.example.android.swoosha
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getstartbutton.setOnClickListener {
+            val  slectionIntent = Intent (this, SelectionActivity::class.java)
+            startActivity(slectionIntent)
+        }
+
     }
 }
